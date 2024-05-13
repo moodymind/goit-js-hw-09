1a3df0f595edfcd23d37ad3c150c9b4d2fd8465c
Austin Miller <vadim.sukhetskii@gmail.com>
2024-05-13 17:40:27 +0300

    final commit
    Added changes to the 2-form.js file. Functions handleSubmit, handleInput and populateForm have been added.

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,1 +1,45 @@
-formData;
+const formData = {
+  email: '',
+  message: '',
+};
+
+const STORAGE_KEY = 'feedback-form-state';
+const form = document.querySelector('.feedback-form');
+populateForm();
+
+form.addEventListener('submit', handleSubmit);
+form.addEventListener('input', handleInput);
+
+function handleSubmit(event) {
+  event.preventDefault();
+
+  const { email, message } = event.target.elements;
+  const inputEmail = email.value.trim();
+  const inputMessage = message.value.trim();
+
+  if (!inputEmail || !inputMessage) {
+    alert('Please fill the empty fields');
+    return;
+  }
+
+  console.log(formData);
+  localStorage.removeItem(STORAGE_KEY);
+  form.reset();
+}
+
+function handleInput(event) {
+  const { name, value } = event.target;
+
+  formData[name] = value.trim();
+
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+function populateForm() {
+  const savedData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+
+  if (savedData) {
+    form.elements.email.value = savedData.email;
+    form.elements.message.value = savedData.message;
+  }
+}
